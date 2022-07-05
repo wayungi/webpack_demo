b9449faeec7199be5f73f9b7b4e966bd8940013a
wayungi <fwayungi@gmail.com>
2022-07-05 12:20:21 +0300

    Add image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,13 @@ module.exports = {
   module: {
 
     rules: [
-
       {
-
         test: /\.css$/i,
-
         use: ['style-loader', 'css-loader'],
-
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
 
     ],
